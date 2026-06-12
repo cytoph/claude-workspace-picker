b6971de6861539fe8648a94aca7857c2d8d6e827
Christoph Mett <christoph.mett@gmail.com>
2026-06-13 01:31:44 +0200

    feat(paths): add Unix environment variable expansion

--- ClaudeWorkspacePicker/Helpers/PathHelper.cs
+++ ClaudeWorkspacePicker/Helpers/PathHelper.cs
@@ -1,12 +1,26 @@
+using System.Text.RegularExpressions;
+
 namespace ClaudeWorkspacePicker.Helpers;
 
-static class PathHelper
+static partial class PathHelper
 {
+    [GeneratedRegex(@"\$\{([^}]+)\}|\$(\w+)")]
+    private static partial Regex UnixEnvironmentVariablePattern();
+
     public static string ExpandPath(string path)
     {
         if (path.Length >= 1 && path[0] == '~' && (path.Length == 1 || path[1] is '/' or '\\'))
-            return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + path[1..];
+            path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + path[1..];
 
-        return Environment.ExpandEnvironmentVariables(path);
+        if (OperatingSystem.IsWindows())
+            return Environment.ExpandEnvironmentVariables(path);
+        else
+            return ExpandUnixEnvironmentVariables(path);
     }
+
+    private static string ExpandUnixEnvironmentVariables(string path) => UnixEnvironmentVariablePattern().Replace(path, match =>
+    {
+        string name = match.Groups[1].Success ? match.Groups[1].Value : match.Groups[2].Value;
+        return Environment.GetEnvironmentVariable(name) ?? match.Value;
+    });
 }
